9dd3caa4523045d6d5d5fad840ed4aceb8509058
RinkeshKalathiya <52659294+RinkeshKalathiya@users.noreply.github.com>
2019-07-09 12:46:44 +0530

    Update mongoscript.js

--- mongoscript.js
+++ mongoscript.js
@@ -1,8 +1,8 @@
-use admin
-db.system.users.remove({})
-db.system.version.remove({})
-db.system.version.insert({ "_id" : "authSchema", "currentVersion" : 3 })
-use admin
+use admin;
+db.system.users.remove({});
+db.system.version.remove({});
+db.system.version.insert({ "_id" : "authSchema", "currentVersion" : 3 });
+use admin;
 db.createUser(
   {
 	user: "root_admin",
@@ -10,11 +10,12 @@ db.createUser(
 	roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
   }
 );
-use sussitDev
-db.sussitDev.insert( { item: "test", qty: 15 } )
+use sussitDev;
+db.sussitDev.insert( { item: "test", qty: 15 } );
 db.createUser(
   {
 	user: "root_DB",
 	pwd: "sussitDev",
 	roles: [ { role: "dbAdmin", db: "sussitDev" }, { role: "readWrite", db: "sussitDev" } ]
   }
+);
